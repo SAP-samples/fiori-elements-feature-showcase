5da228893f7cfca117850fa99dc337e3ace4a296
Marten Schiwek <marten.schiwek@sap.com>
2025-09-22 19:24:25 +0200

    Moved controller extension

--- app/listreport-objectpage/webapp/ext/controller/ControllerExtension.d.ts
+++ app/listreport-objectpage/webapp/ext/controller/ControllerExtension.d.ts
@@ -7,22 +7,31 @@ declare module 'sap/ui/core/mvc/ControllerExtension' {
   import Controller from 'sap/ui/core/mvc/Controller';
   import EditFlow from 'sap/fe/core/controllerextensions/EditFlow';
   import IntentBasedNavigation from 'sap/fe/core/controllerextensions/IntentBasedNavigation';
-  import ViewState from 'sap/fe/core/controllerextensions/ViewState';
+  import MessageHandler from 'sap/fe/core/controllerextensions/MessageHandler';
+  import Paginator from 'sap/fe/core/controllerextensions/Paginator';
+  import Recommendations from 'sap/fe/core/controllerextensions/Recommendations';
   import Routing from 'sap/fe/core/controllerextensions/Routing';
+  import Share from 'sap/fe/core/controllerextensions/Share';
+  import ViewState from 'sap/fe/core/controllerextensions/ViewState';
+  import CoreElement from 'sap/ui/core/Element';
 
   export default class ControllerExtension<ExtensionAPI> {
-    override?: Overrides;
     base: {
       getExtensionAPI(): ExtensionAPI;
       getView(): View;
       getAppComponent(): AppComponent;
+      byId(id: string): CoreElement
     };
   }
 
   export type Overrides<GenericController extends Controller = Controller> = {
     editFlow?: Partial<EditFlow>;
     intentBasedNavigation?: Partial<IntentBasedNavigation>;
+    messageHandler?: Partial<MessageHandler>;
+    paginator?: Partial<Paginator>;
+    recommendations?: Partial<Recommendations>;
     routing?: Partial<Routing>;
+    share?: Partial<Share>;
     viewState?: Partial<ViewState>;
   } & Partial<GenericController>;
 }
